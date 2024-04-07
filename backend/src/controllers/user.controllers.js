1bd1e370f295537cf9a074a4da85d187aa7dbcf4
Khushi Vora <vorakhushi66@gmail.com>
2024-04-07 15:41:58 +0530

    [backend]-demo of edit user data.

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -8,4 +8,15 @@ const postEditProfilePic = async (req, res) => {
   res.status(200).send(isSet);
 };
 
+// const postEditUsername = async (req, res) => {
+//   const { userId } = req.params;
+//   const { username } = req.body;
+//   const isSet = await userService.editUserData(userId, username, "username");
+//   edutUserData(userId, value, fliedToEdit);
+//   // in service-----------
+//   // user[fieldToEdit] = value;
+//   // user.save()
+//   res.status(200).send(isSet);
+// };
+
 module.exports = { postEditProfilePic };
